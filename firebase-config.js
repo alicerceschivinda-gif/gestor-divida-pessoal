a3ee4c4ebd9d322a7902d4792b24abc6ae5cd432
alicerceschivinda-gif <alicerceschivinda@gmail.com>
2026-07-04 21:41:51 +0100

    Adicionar configuração Firebase em modo de espera

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "COLE_AQUI",
+  authDomain: "COLE_AQUI",
+  projectId: "COLE_AQUI",
+  storageBucket: "COLE_AQUI",
+  messagingSenderId: "COLE_AQUI",
+  appId: "COLE_AQUI"
+};
